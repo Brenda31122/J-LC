0d9fb9af5dca19ae4dcdab47a67bc6010da186b1
danny-gallo <cangreburguito77@gmail.com>
2025-01-16 00:37:48 -0600

    corregido fragmento inicio_c

--- app/src/main/java/com/daniel/tienda/Cliente/MainActivityCliente.kt
+++ app/src/main/java/com/daniel/tienda/Cliente/MainActivityCliente.kt
@@ -1,13 +1,51 @@
 package com.daniel.tienda.Cliente
 
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import com.daniel.tienda.Cliente.Nav_Fragments_Cliente.FragmentInicio_C
 import com.daniel.tienda.R
+import com.daniel.tienda.databinding.ActivityMainClienteBinding
+import com.daniel.tienda.databinding.ActivityMainVendedorBinding
+import com.google.android.material.navigation.NavigationView
 
 
-class MainActivityCliente : AppCompatActivity() {
+class MainActivityCliente : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var binding: ActivityMainClienteBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_cliente)
+        binding = ActivityMainClienteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        binding.navigationView.setNavigationItemSelectedListener(this)
+
+        val  toggle = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayout,
+            toolbar,
+            R.string.open_drawer,
+            R.string.close_drawer
+        )
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        replaceFragment(FragmentInicio_C())
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.navFragment,fragment)
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
     }
 }
