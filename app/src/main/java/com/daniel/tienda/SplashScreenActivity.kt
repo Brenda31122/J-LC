0d0c8696ccc3db81be42e72735f04d27d7272c82
danny-gallo <cangreburguito77@gmail.com>
2025-01-16 02:27:53 -0600

    funcion vendedor

--- app/src/main/java/com/daniel/tienda/SplashScreenActivity.kt
+++ app/src/main/java/com/daniel/tienda/SplashScreenActivity.kt
@@ -7,12 +7,23 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.daniel.tienda.Cliente.MainActivityCliente
 import com.daniel.tienda.Vendedor.MainActivityVendedor
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        firebaseAuth = FirebaseAuth.getInstance()
 
         verBienvenida()
     }
@@ -23,10 +34,37 @@ class SplashScreenActivity : AppCompatActivity() {
             }
 
             override fun onFinish() {
-                startActivity(Intent(applicationContext, MainActivityVendedor ::class.java))
-                finishAffinity()
+                comprobarTipoUsuario()
             }
 
         } .start()
     }
+
+    private fun comprobarTipoUsuario(){
+    val  firebaseUser = firebaseAuth.currentUser
+    if (firebaseUser == null){
+        startActivity(Intent(this, MainActivityVendedor::class.java))
+    }else {
+        val reference = FirebaseDatabase.getInstance().getReference("Usuarios")
+        reference.child(firebaseUser.uid)
+            .addListenerForSingleValueEvent(object : ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val tipoU = snapshot.child("tipoUsuario").value
+
+                    if (tipoU=="vendedor"){
+                        startActivity(Intent(this@SplashScreenActivity, MainActivityVendedor::class.java))
+                        finishAffinity()
+                    }else if (tipoU == "cliente"){
+                        startActivity(Intent(this@SplashScreenActivity,MainActivityCliente::class.java))
+                        finishAffinity()
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+            })
+
+    }
+}
 }
