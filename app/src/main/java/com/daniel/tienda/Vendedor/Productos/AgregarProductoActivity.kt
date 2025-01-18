9fac39c177452edf157f0f6607c0f6d865e95ab0
brenda <lorabrenda_30@hotmail.com>
2025-01-17 22:11:54 -0600

    AgregarProductos

---
+++ app/src/main/java/com/daniel/tienda/Vendedor/Productos/AgregarProductoActivity.kt
@@ -0,0 +1,12 @@
+package com.daniel.tienda.Vendedor.Productos
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.daniel.tienda.R
+
+class AgregarProductoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agregar_producto2)
+    }
+}
