aa2eb7f09c1fd5c392fddbee700f5994a65b77bf
brenda <lorabrenda_30@hotmail.com>
2025-01-18 22:38:37 -0600

    dandole funcionamiento al boton eliminar imagen

--- app/src/main/java/com/daniel/tienda/Adaptadores/AdaptadorImagenSeleccionada.kt
+++ app/src/main/java/com/daniel/tienda/Adaptadores/AdaptadorImagenSeleccionada.kt
@@ -1,13 +1,17 @@
 package com.daniel.tienda.Adaptadores
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.bumptech.glide.Glide
 import com.daniel.tienda.Modelos.ModeloImagenSeleccionada
+import com.daniel.tienda.R
 import com.daniel.tienda.databinding.ItemImagenesSeleccionadasBinding
+
 
 
 class AdaptadorImagenSeleccionada (
@@ -25,13 +29,32 @@ class AdaptadorImagenSeleccionada (
 
     }
 
+    @SuppressLint("CheckResult")
     override fun onBindViewHolder(holder: HolderImagenSeleccionada, position: Int) {
         val modelo = imagenesSelecArrayList[position]
 
         val imagenUri = modelo.imageUri
+
+
+        try {
+            Glide.with(context)
+                .load(imagenUri)
+                .placeholder(R.drawable.item_imagen)
+                .into(holder.imagenItem)
+
+
+        }catch (e:Exception){
+
+        }
+        //evento para eliminar imagen de lista
+
+        holder.btn_borrar.setOnClickListener {
+            imagenesSelecArrayList.remove(modelo)
+            notifyDataSetChanged()
+        }
     }
     inner class HolderImagenSeleccionada (itemView : View) : ViewHolder(itemView){
-      var item_imagen = binding.itemImagen
+      var imagenItem = binding.imagenItem
       var btn_borrar = binding.borrarItem
 
 
