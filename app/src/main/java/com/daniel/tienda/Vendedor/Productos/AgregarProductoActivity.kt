933cae3763f5ce75859a4338efb30ca12bbbe2a6
brenda <lorabrenda_30@hotmail.com>
2025-01-18 21:07:07 -0600

    Camara y galeria implementadas

--- app/src/main/java/com/daniel/tienda/Vendedor/Productos/AgregarProductoActivity.kt
+++ app/src/main/java/com/daniel/tienda/Vendedor/Productos/AgregarProductoActivity.kt
@@ -1,14 +1,49 @@
 package com.daniel.tienda.Vendedor.Productos
 
+import android.app.Activity
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.daniel.tienda.R
+import com.daniel.tienda.databinding.ActivityAgregarProducto2Binding
+import com.github.dhaval2404.imagepicker.ImagePicker
 
 class AgregarProductoActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityAgregarProducto2Binding
+    private var imagenUri : Uri?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_agregar_producto2)
+        binding = ActivityAgregarProducto2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.imgAgregarProducto.setOnClickListener {
+            seleccionarImg()
+        }
     }
+    private fun seleccionarImg(){
+        ImagePicker.with(this)
+            .crop()
+            .compress(1024)
+            .maxResultSize(1080,1080)
+            .createIntent { intent->
+             resultadoImg.launch(intent)
+
+            }
+
+    }
+    private val resultadoImg =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){resultado ->
+            if (resultado.resultCode== Activity.RESULT_OK){
+                val data = resultado.data
+                imagenUri= data!!.data
+                binding.imgAgregarProducto.setImageURI(imagenUri)
+            }else{
+                Toast.makeText(this,"Accion cancelada",Toast.LENGTH_SHORT).show()
+
+            }
+        }
 }
