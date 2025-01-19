bc8019a9dc378dc879f6fbe4c0310356adbc9471
brenda <lorabrenda_30@hotmail.com>
2025-01-18 22:53:12 -0600

    Ya se pueden seleccionar varias imagenes de galeria

--- app/src/main/java/com/daniel/tienda/Vendedor/Productos/AgregarProductoActivity.kt
+++ app/src/main/java/com/daniel/tienda/Vendedor/Productos/AgregarProductoActivity.kt
@@ -6,6 +6,9 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.daniel.tienda.Adaptadores.AdaptadorImagenSeleccionada
+import com.daniel.tienda.Constantes
+import com.daniel.tienda.Modelos.ModeloImagenSeleccionada
 import com.daniel.tienda.R
 import com.daniel.tienda.databinding.ActivityAgregarProducto2Binding
 import com.github.dhaval2404.imagepicker.ImagePicker
@@ -14,16 +17,29 @@ class AgregarProductoActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAgregarProducto2Binding
     private var imagenUri : Uri?=null
+    private lateinit var imagenSelecArrayList :ArrayList<ModeloImagenSeleccionada>
+    private lateinit var adaptadorImagenSele: AdaptadorImagenSeleccionada
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAgregarProducto2Binding.inflate(layoutInflater)
         setContentView(binding.root)
+       imagenSelecArrayList = ArrayList()
 
         binding.imgAgregarProducto.setOnClickListener {
             seleccionarImg()
         }
+
+        cargarImagenes()
+
+
     }
+
+    private fun cargarImagenes() {
+        adaptadorImagenSele = AdaptadorImagenSeleccionada(this,imagenSelecArrayList)
+        binding.RVImagenesProducto.adapter = adaptadorImagenSele
+    }
+
     private fun seleccionarImg(){
         ImagePicker.with(this)
             .crop()
@@ -40,7 +56,11 @@ class AgregarProductoActivity : AppCompatActivity() {
             if (resultado.resultCode== Activity.RESULT_OK){
                 val data = resultado.data
                 imagenUri= data!!.data
-                binding.imgAgregarProducto.setImageURI(imagenUri)
+                val tiempo = "${Constantes().obtenerTiempoD()}"
+
+                val modeloImSel = ModeloImagenSeleccionada(tiempo,imagenUri,null,false)
+                imagenSelecArrayList.add(modeloImSel)
+                cargarImagenes()
             }else{
                 Toast.makeText(this,"Accion cancelada",Toast.LENGTH_SHORT).show()
 
